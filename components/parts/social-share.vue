9adba8390c9d1920f43f075ca555e474e2b96f61
Alex Asche <alexcasche@gmail.com>
2021-05-11 15:34:47 -0400

    adds in homepage section

---
+++ components/parts/social-share.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="social-share">
+    Social Sharing......
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'partSocialShare'
+}
+</script>
+
+<style lang="scss">
+  .social-share {
+
+  }
+</style>
---
+++ components/sections/carousel-hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <base-carousel class="carousel-hero"
+    :options="{
+      infinite: true,
+      dots: true,
+      controls: true
+    }"
+  >
+    <div>slide 1</div>
+    <div>slide 2</div>
+  </base-carousel>
+</template>
+
+<script>
+export default {
+  name: 'sectionCarouselHero',
+  props: {
+    slides: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
