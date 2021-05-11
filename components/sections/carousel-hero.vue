fefd35d757064e1be47b47d8ed6516d8a2cf3da4
Alex Asche <alexcasche@gmail.com>
2021-05-11 17:27:52 -0400

    homepage carousel

--- components/sections/carousel-hero.vue
+++ components/sections/carousel-hero.vue
@@ -4,6 +4,8 @@
       <base-carousel class="carousel-hero__carousel"
         :options="{
           infinite: true,
+          draggable: false,
+          lazyload: true,
           dots: true,
           dotsClass: 'carousel-hero__dots',
           controls: true
@@ -19,6 +21,19 @@
               :src="slide.image.src"
               :alt="slide.image.alt"
             />
+            <div class="carousel-hero__slide-wrapper">
+              <div class="carousel-hero__slide-content">
+                <h2 class="carousel-hero__slide-heading"
+                  v-if="slide.heading"
+                  v-html="slide.heading"
+                />
+                <base-a class="carousel-hero__slide-button button is-primary"
+                  v-if="slide.button"
+                  :href="slide.button.url"
+                  v-html="slide.button.text"
+                />
+              </div>
+            </div>
           </div>
         </div>
         <button class="carousel-hero__dot"
@@ -26,6 +41,20 @@
           :key="`slide-${index}`"
           slot="customPaging"
         />
+        <button class="carousel-hero__control is-prev"
+          slot="prevArrow"
+        >
+          <base-svg class="carousel-hero__control-icon"
+            name="chevron"
+          />
+        </button>
+        <button class="carousel-hero__control is-next"
+          slot="nextArrow"
+        >
+          <base-svg class="carousel-hero__control-icon"
+            name="chevron"
+          />
+        </button>
       </base-carousel>
     </div>
   </div>
@@ -51,6 +80,7 @@ export default {
   }
   .carousel-hero__slide {
     position: relative;
+    display: flex !important;
     padding-bottom: 30%;
   }
   .carousel-hero__slide-inner {
@@ -64,14 +94,38 @@ export default {
   }
   .carousel-hero__slide-image {
     object-fit: cover;
+    object-position: center center;
+  }
+  .carousel-hero__slide-wrapper {
+    position: absolute;
+    top: 50%;
+    left: 10%;
+    transform: translateY(-50%);
+    width: 80%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    margin: 0 auto;
+    z-index: 1;
+  }
+  .carousel-hero__slide-content {
+    width: 50%;
+  }
+  .carousel-hero__slide-heading {
+    color: $color-white;
+    font-size: 3.2rem;
+    line-height: 1.25;
+    margin-top: 0;
   }
   .carousel-hero__dots {
-    width: 300px;
+    width: 140px;
     display: flex !important;
     justify-content: center;
     gap: 4px;
     flex-wrap: wrap;
-    margin: 20px auto 0;
+    margin: 10px auto 0;
   }
   .carousel-hero__dot {
     @include unset-button;
@@ -88,6 +142,33 @@ export default {
     }
     &:hover {
       opacity: 0.75;
+    }
+  }
+  .carousel-hero__control {
+    @include unset-button;
+    position: absolute;
+    bottom: 0;
+    cursor: pointer;
+    &.is-prev {
+      left: calc(50% - 80px);
+      transform: translateX(-50%);
+    }
+    &.is-next {
+      right: calc(50% - 80px);
+      transform: translateX(50%);
+    }
+  }
+  .carousel-hero__control-icon {
+    height: 16px;
+    color: $color-primary;
+    &:hover {
+      opacity: 0.75;
+    }
+    .is-prev & {
+      transform: rotate(90deg);
+    }
+    .is-next & {
+      transform: rotate(-90deg);
     }
   }
 </style>
